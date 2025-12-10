469a3e80b514f63bc954c585f43f8b4de7c971e2
Nikhil <hdixit07@gmail.com>
2025-12-10 15:22:49 +0530

    Time: 20 ms (57.22%) | Memory: 31.9 MB (82.78%) - LeetSync

---
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countPermutations(self, complexity):
+        MOD = 10**9 + 7
+        n = len(complexity)
+        for i in range(1, n):
+            if complexity[i] <= complexity[0]:
+                return 0
+        ans = 1
+        for i in range(1, n):
+            ans = ans * i % MOD
+        return ans
